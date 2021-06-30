34f6155e475f723b30aa3ab86d414d54047f4766
bohunn <borun.p@hotmail.com>
2021-06-30 23:12:07 +0200

    rework ensuring

--- controllers/database_controller.go
+++ controllers/database_controller.go
@@ -18,20 +18,20 @@ package controllers
 
 import (
 	"context"
+	githubcomv1alpha1 "github.com/bohunn/thedealer/api/v1alpha1"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/discovery"
+	"k8s.io/client-go/rest"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
-	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/log"
-	"sigs.k8s.io/controller-runtime/pkg/predicate"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
-
-	githubcomv1alpha1 "github.com/bohunn/thedealer/api/v1alpha1"
+	"strings"
 )
 
 //getDiscoveryClient returns a discovery client for the current reconciler
@@ -77,6 +77,7 @@ func (r *DatabaseReconciler) Reconcile(ctx context.Context, req ctrl.Request) (c
 		return reconcile.Result{}, err
 	}
 
+	//-- Checking if config map has been changed
 	configMapChanged, err := r.ensureLatestConfigMap(instance)
 	if err != nil {
 		return reconcile.Result{}, err
@@ -85,47 +86,66 @@ func (r *DatabaseReconciler) Reconcile(ctx context.Context, req ctrl.Request) (c
 	if err != nil {
 		return reconcile.Result{}, err
 	}
+
+	//-- Checking if Service has been changed
 	serviceChanged, err := r.ensureServiceForCr(instance)
 
 	if err != nil {
 		return reconcile.Result{}, err
 	}
-	reqLogger.Info("checking if service changed", "serviceChanged", serviceChanged)
-	//cfg, err := config.GetConfig()
-	//if err != nil {
-	//	log.Error(err, "")
-	//	os.Exit(1)
-	//}
-	//
-	//discoveryClient, err := discovery.NewDiscoveryClientForConfig(cfg)
-	//if err != nil {
-	//	log.Error(err, "Unable to create discovery client")
-	//	os.Exit(1)
-	//}
-	// your logic here
+	err = r.ensureLatestPod(instance, serviceChanged)
+
+	if err != nil {
+		return reconcile.Result{}, err
+	}
+
+	config, err := ctrl.GetConfig()
+	if err == nil && config != nil {
+		dclient, err := getDiscoveryClient(config)
+		if err == nil && dclient != nil {
+			apiGroupList, err := dclient.ServerGroups()
+			if err != nil {
+				reqLogger.Info("Error while querying ServerGroups, assuming we're on Vanilla Kubernetes")
+			} else {
+				for i := 0; i < len(apiGroupList.Groups); i++ {
+					if strings.HasSuffix(apiGroupList.Groups[i].Name, ".openshift.io") {
+						//isOpenShift = true
+						reqLogger.Info("Detected Openshift ")
+						break
+					}
+				}
+			}
+		} else {
+			reqLogger.Info("Cannot retrieve a DiscoveryClient, assuming Kubernetes")
+		}
+	}
 
 	return ctrl.Result{}, nil
 }
 
-func ignoreDeletionPredicate() predicate.Predicate {
-	reqLogger := log.Log //log.FromContext(ctx, "Request.Namespace", req.Namespace, "Request.Name", req.Name)
-	reqLogger.Info("ignoreDeletionPredicate")
-
-	return predicate.Funcs{
-		UpdateFunc: func(event event.UpdateEvent) bool {
-			return event.ObjectOld.GetGeneration() != event.ObjectNew.GetGeneration()
-		},
-		DeleteFunc: func(deleteEvent event.DeleteEvent) bool {
-			return !deleteEvent.DeleteStateUnknown
-		},
-	}
+// getDiscoveryClient returns a discovery client for the current reconciler
+func getDiscoveryClient(config *rest.Config) (*discovery.DiscoveryClient, error) {
+	return discovery.NewDiscoveryClientForConfig(config)
 }
+
+//func ignoreDeletionPredicate() predicate.Predicate {
+//	reqLogger := log.Log //log.FromContext(ctx, "Request.Namespace", req.Namespace, "Request.Name", req.Name)
+//	reqLogger.Info("ignoreDeletionPredicate")
+//
+//	return predicate.Funcs{
+//		UpdateFunc: func(event event.UpdateEvent) bool {
+//			return event.ObjectOld.GetGeneration() != event.ObjectNew.GetGeneration()
+//		},
+//		DeleteFunc: func(deleteEvent event.DeleteEvent) bool {
+//			return !deleteEvent.DeleteStateUnknown
+//		},
+//	}
+//}
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *DatabaseReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&githubcomv1alpha1.Database{}).
-		WithEventFilter(ignoreDeletionPredicate()).
 		Complete(r)
 }
 
@@ -156,29 +176,31 @@ func newServiceForCr(cr *githubcomv1alpha1.Database) *v1.Service {
 
 func (r *DatabaseReconciler) ensureServiceForCr(instance *githubcomv1alpha1.Database) (bool, error) {
 	logger := log.Log.WithName("ensureServiceForCr")
-	logger.Info("Creating a Service for ", instance)
 	service := newServiceForCr(instance)
 
 	foundService := &v1.Service{}
 
 	err := r.Client.Get(context.TODO(), types.NamespacedName{Name: service.Name}, foundService)
-	logger.Info("Finding service", "foundService", foundService, "err", err)
+
+	logger.Info("Finding service", "err", err)
 	if err != nil && errors.IsNotFound(err) {
 		err = r.Client.Create(context.TODO(), service)
-	} else if err != nil && errors.IsAlreadyExists(err) {
-		err = r.Client.Update(context.TODO(), service)
+	}
+	if err != nil && errors.IsAlreadyExists(err) {
+		//err = r.Client.Update(context.TODO(), service)
+		err = nil
 	}
 	if err != nil {
 		return false, err
 	}
 
-	if service.Kind != foundService.Kind {
-		err = r.Client.Update(context.TODO(), service)
-		if err != nil {
-			return false, err
-		}
-		return true, nil
-	}
+	//if service.Kind != foundService.Kind {
+	//	err = r.Client.Update(context.TODO(), service)
+	//	if err != nil {
+	//		return false, err
+	//	}
+	//	return true, nil
+	//}
 
 	return false, nil
 
@@ -221,6 +243,68 @@ func newPodForCR(cr *githubcomv1alpha1.Database) *v1.Pod {
 				},
 			},
 		},
+	}
+}
+
+func (r *DatabaseReconciler) ensureLatestService(instance *githubcomv1alpha1.Database) (bool, error) {
+	service := newService(instance)
+
+	// Set Database instance as the owner and controller
+	if err := controllerutil.SetControllerReference(instance, service, r.Scheme); err != nil {
+		return false, err
+	}
+
+	// Check if this ConfigMap already exists
+	foundService := &v1.Service{}
+	err := r.Client.Get(context.TODO(), types.NamespacedName{Name: service.Name, Namespace: service.Namespace}, foundService)
+	if err != nil && errors.IsNotFound(err) {
+		err = r.Client.Create(context.TODO(), service)
+		if errors.IsAlreadyExists(err) {
+			//err = r.Client.Update(context.TODO(), service)
+			err = nil
+		}
+		if err != nil {
+			return false, err
+		}
+	} else if err != nil {
+		return false, err
+	}
+
+	//if foundService.Kind != service.Kind {
+	//	err = r.Client.Update(context.TODO(), service)
+	//	if err != nil {
+	//		return false, err
+	//	}
+	//	return true, nil
+	//}
+	return false, nil
+
+}
+
+func newService(cr *githubcomv1alpha1.Database) *v1.Service {
+	labels := map[string]string{
+		"app": cr.Name,
+	}
+	return &v1.Service{
+		TypeMeta: metav1.TypeMeta{
+			Kind: "Service",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      cr.Name + "-service",
+			Namespace: cr.Namespace,
+			Labels:    labels,
+		},
+		Spec: v1.ServiceSpec{
+			Ports: []v1.ServicePort{
+				{
+					Protocol: v1.ProtocolTCP,
+					Port:     8090,
+					//NodePort: 8090,
+				},
+			},
+			Type: "NodePort",
+		},
+		Status: v1.ServiceStatus{},
 	}
 }
 
